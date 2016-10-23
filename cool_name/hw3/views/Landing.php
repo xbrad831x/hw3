11594462be05c3a9961b4a1e4db99f08d59c6770
xbrad831x <mercadobrandon33@yahoo.com>
2016-10-23 03:20:36 -0700

    Added sorted lists for Most viewed and Highest Rated

--- cool_name/hw3/views/Landing.php
+++ cool_name/hw3/views/Landing.php
@@ -7,29 +7,44 @@ class Landing extends View {
 
 	public function __construct()
 	    {
-	        $this->genre_dropdown_helper = new Helper();   
+	        $this->helper = new Helper();   
 	    }
 
 	public function render() {
         
         $this->renderHeader("Five Thousand Characters");
+		$this->helper->populate_genre_dropdown();
         ?>
 
         <h1>Five Thousand Characters</h1>
-        <a href="write_something.php">Write Something!</a>
-        <h2>Check out what people are writing...</h2>
+		<a href="write_something.php">Write Something!</a>
+		<h2>Check out what people are writing...<h2>
 
-        <form>
-            <input type="text" id="title_filter" name="filter_text" placeholder="Phrase Filter">
-            <select id="select_filter" name="genre_filter">
-                <option selected="selected" value="All Genres">All Genres</option>
-                <?php $this->genre_dropdown_helper->generate_genre_dropdown(); ?>
-            </select>
-        </form>
+		<form>
+			<input type="text" id="title_filter" name="filter_text" placeholder="Phrase Filter">
+			<select id="select_filter" name="genre_filter">
+			<option selected="selected" value="All Genres">All Genres</option>
+			<?php $this->helper->generate_genre_dropdown(); ?>
+			</select>
+		</form>
 
-        <h3>Highest Rated</h3>
-        <h3>Most Viewed</h3>
-        <h3>Newest</h3>
+		<h3>Highest Rated</h3>
+
+		<ol type="1">
+		<?php $this->helper->generate_top_rated(); ?>
+		</ol>
+
+		<h3>Most Viewed</h3>
+
+		<ol type="1">
+		<?php $this->helper->generate_top_views(); ?>
+		</ol>
+
+		<h3>Newest</h3>
+
+		<ol type="1">
+
+		</ol>
         
         <?php 
         $this->renderFooter();
