c932a761c8ccd78acb4ebd83f89e34044a34e26c
xbrad831x <mercadobrandon33@yahoo.com>
2016-10-22 22:54:45 -0700

    Changes to landing page and DB Creation

--- cool_name/hw3/configs/CreateDB.php
+++ cool_name/hw3/configs/CreateDB.php
@@ -1,5 +1,5 @@
 <?php
-require("configs.php");
+require_once("configs.php");
 $DB_config = new Configs();
 $con = mysqli_connect($DB_config->host,$DB_config->username,$DB_config->password);
 mysqli_query($con, "CREATE DATABASE story");
@@ -8,7 +8,8 @@ mysqli_query($con, "CREATE TABLE story (Identifier VARCHAR(30) PRIMARY KEY,
 										Title VARCHAR(30),
 										Author VARCHAR(30),
 										Text VARCHAR(5000),
-										Date DATE)");
+										Date timestamp,
+										Views INT UNSIGNED NOT NULL)");
 mysqli_query($con, "CREATE TABLE genre (Animal Varchar(30),
 										Funny Varchar(30),
 										Cute Varchar(30),
@@ -19,10 +20,5 @@ mysqli_query($con, "CREATE TABLE ratings (Identifier VARCHAR(30) PRIMARY KEY,
 										  Num_Of_Ratings INT, 
 										  Sum_Of_Ratings INT)");
 										  
-$result = mysqli_query($con, "SELECT * FROM genre");
-while ($column = mysqli_fetch_field($result))
-{
-echo $column->name;
-}
 
 ?>
--- cool_name/hw3/views/Landing.php
+++ cool_name/hw3/views/Landing.php
@@ -1,10 +1,17 @@
 <?php 
 /*namespace cool_name\hw3\views\landing;*/
-require ("View.php");
+require_once("View.php");
+require_once("/helpers/Helper.php");
 
 class Landing extends View {
+
+	public function __construct()
+	    {
+	        $this->genre_dropdown_helper = new Helper();   
+	    }
+
 	public function render() {
-	
+	$this->genre_dropdown_helper->populate_genre_dropdown();
 	?>
 		<!DOCTYPE html>
 	<html>
@@ -17,6 +24,22 @@ class Landing extends View {
 	<body>
 	<h1>Five Thousand Characters</h1>
 	<a href="write_something.php">Write Something!</a>
+	<h2>Check out what people are writing...<h2>
+
+	<form>
+		<input type="text" id="title_filter" name="filter_text" placeholder="Phrase Filter">
+		<select id="select_filter" name="genre_filter">
+		<option selected="selected" value="All Genres">All Genres</option>
+		<?php $this->genre_dropdown_helper->generate_genre_dropdown(); ?>
+		</select>
+
+	</form>
+
+	<h3>Highest Rated</h3>
+
+	<h3>Most Viewed</h3>
+
+	<h3>Newest</h3>
 
 	</body>
 
