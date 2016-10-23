9daa51ef76189e183b4f342dd4a0ff666cf33eaa
xbrad831x <mercadobrandon33@yahoo.com>
2016-10-23 15:58:31 -0700

    Added sorting

--- cool_name/hw3/views/helpers/Helper.php
+++ cool_name/hw3/views/helpers/Helper.php
@@ -61,6 +61,32 @@ class Helper {
 				echo '<li><a href="read_a_story.php?title='.$row["Title"].'&identifier='.$row["Identifier"].'">'.$row["Title"].'</a></li>';
 			}
 	}
+	
+	public function generate_top_viewed_filtered_Text($text) {
+		$this->model->db_connect();
+		$result = $this->model->sql_query("SELECT Title, Identifier FROM story WHERE Title LIKE '%$text%' ORDER BY Views DESC LIMIT 10");
+		while($row = mysqli_fetch_assoc($result))
+			{
+				echo '<li><a href="read_a_story.php?title='.$row["Title"].'&identifier='.$row["Identifier"].'">'.$row["Title"].'</a></li>';
+			}
+	}
+
+	public function generate_top_rated_filtered_Text($text) {
+		$this->model->db_connect();
+		$result = $this->model->sql_query("SELECT Title, Identifier FROM ratings WHERE Title LIKE '%$text%' ORDER BY Sum_Of_Ratings / Num_Of_Ratings DESC LIMIT 10");
+		while($row = mysqli_fetch_assoc($result))
+			{
+				echo '<li><a href="read_a_story.php?title='.$row["Title"].'&identifier='.$row["Identifier"].'">'.$row["Title"].'</a></li>';
+			}
+	}
+
+	public function generate_newest_filtered_Text($text) {
+		$this->model->db_connect();
+		$result = $this->model->sql_query("SELECT Title, Identifier FROM story WHERE Title LIKE '%$text%' ORDER BY Date DESC LIMIT 10");
+		while($row = mysqli_fetch_assoc($result))
+			{
+				echo '<li><a href="read_a_story.php?title='.$row["Title"].'&identifier='.$row["Identifier"].'">'.$row["Title"].'</a></li>';
+			}
 
 }
 ?>
