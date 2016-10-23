140c6f2d2a6fccf0dc0bbdba710bbc56cda8fa15
xbrad831x <mercadobrandon33@yahoo.com>
2016-10-23 03:09:37 -0700

    Added a lot of functions

--- cool_name/hw3/models/Model.php
+++ cool_name/hw3/models/Model.php
@@ -20,9 +20,52 @@ class Model {
 	public function sql_insert_story($identifier, $title, $author, $genres, $text) {
 		$my_date = date("Y-m-d H:i:s");
 		mysqli_query($this->con,"INSERT INTO story (Identifier, Title, Author, Text, Date) VALUES ('$identifier','$title','$author','$text','$my_date')");
-		foreach($genres as $genre)    {
+		foreach($genres as $genre)
+		{
 			mysqli_query($this->con, "INSERT INTO genre ($genre) VALUES ('$identifier')");
 		}
+		mysqli_query($this->con,"INSERT INTO ratings (Identifier, Title) VALUES ('$identifier', '$title')");
+	}
+
+	public function get_author($identifier) {
+		$result = mysqli_query($this->con, "SELECT * FROM story WHERE Identifier='$identifier'");
+		$row = mysqli_fetch_assoc($result);
+		return $row["Author"];
+	}
+
+	public function get_date($identifier) {
+		$result = mysqli_query($this->con, "SELECT * FROM story WHERE Identifier='$identifier'");
+		$row = mysqli_fetch_assoc($result);
+		return $row["Date"];
+	}
+
+	public function get_text($identifier) {
+		$result = mysqli_query($this->con, "SELECT * FROM story WHERE Identifier='$identifier'");
+		$row = mysqli_fetch_assoc($result);
+		return $row["Text"];
+
+	}
+
+	public function inc_view($identifier) {
+		mysqli_query($this->con, "UPDATE story SET Views = Views+1 WHERE Identifier='$identifier'");
+	}
+
+	public function set_ratings($identifier, $rating) {
+		mysqli_query($this->con, "UPDATE ratings SET Num_Of_Ratings = Num_Of_Ratings+1 WHERE Identifier='$identifier'");
+		mysqli_query($this->con, "UPDATE ratings SET Sum_Of_Ratings = Sum_Of_Ratings+'$rating' WHERE Identifier='$identifier'");
+	}
+
+	public function get_average_ratings($identifier) {
+		$result = mysqli_query($this->con, "SELECT * FROM ratings WHERE Identifier='$identifier' ");
+		$row = mysqli_fetch_assoc($result);
+		if($row["Num_Of_Ratings"] !=0)
+			{
+				return $row["Sum_Of_Ratings"] / $row["Num_Of_Ratings"];
+			}
+		else
+			{
+				return 0;
+			}
 	}
 
 }
