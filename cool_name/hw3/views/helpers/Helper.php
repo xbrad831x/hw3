608c12c43033588d967e75a8d75c91124791b788
xbrad831x <mercadobrandon33@yahoo.com>
2016-10-23 03:22:46 -0700

    Added 2 functions to Helper class

--- cool_name/hw3/views/helpers/Helper.php
+++ cool_name/hw3/views/helpers/Helper.php
@@ -25,6 +25,25 @@ class Helper {
 			}
 		}
 	}
+	
+	public function generate_top_views() {
+		$this->model->db_connect();
+		$result = $this->model->sql_query("SELECT Title, Identifier FROM story ORDER BY Views DESC LIMIT 10");
+		while($row = mysqli_fetch_assoc($result))
+			{
+				echo '<li><a href="read_a_story.php?title='.$row["Title"].'&identifier='.$row["Identifier"].'">'.$row["Title"].'</a></li>';
+			}
+
+	}
+
+	public function generate_top_rated() {
+		$this->model->db_connect();
+		$result = $this->model->sql_query("SELECT Identifier, Title FROM ratings ORDER BY Sum_Of_Ratings / Num_Of_Ratings DESC LIMIT 10");
+		while($row = mysqli_fetch_assoc($result))
+			{
+				echo '<li><a href="read_a_story.php?title='.$row["Title"].'&identifier='.$row["Identifier"].'">'.$row["Title"].'</a></li>';
+			}
+	}
 
 }
 ?>
