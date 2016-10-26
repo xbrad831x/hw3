4ff4712e4e733e51e5915b0ff2a26737bf6cce53
a-chen01 <albert.chen01@sjsu.edu>
2016-10-26 14:49:05 -0700

    Added More Controllers

---
+++ cool_name/hw3/controllers/FilterController.php
@@ -0,0 +1,152 @@
+<?php
+require_once("../configs/configs.php");
+require_once("../controllers/Controller.php");
+require_once("../models/Story.php");
+
+class FilterController extends Controller{
+    
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function get_views() {
+        $results = mysqli_query($this->con, 
+            "SELECT 
+                Title, 
+                Identifier 
+            FROM story 
+            ORDER BY Views 
+            DESC LIMIT 10;"
+        );
+        if ($results=="") {
+            return null;
+        }
+        return $results;
+        //render_list($results);
+    }
+
+	public function get_rated() {
+		$results = mysqli_query($this->con, 
+            "SELECT 
+                Identifier, 
+                Title 
+            FROM ratings
+            ORDER BY Sum_Of_Ratings/Num_Of_Ratings 
+            DESC LIMIT 10;"
+        );
+        if ($results=="") {
+            return null;
+        }
+        return $results;
+        //render_list($results);
+	}
+
+	public function get_newest() {
+		$results = mysqli_query($this->con, 
+             "SELECT 
+                story.Identifier, 
+                story.Title 
+            FROM story 
+            ORDER BY Date 
+            DESC LIMIT 10;"
+        );
+        if ($results=="") {
+            return null;
+        }
+        return $results;
+        //render_list($results);
+	}
+	
+	public function get_filtered_views($text, $genre) {
+        $filter = "";
+        if ($text != "" && $genre != "") {
+            $filter = "story.Title LIKE '%$text%' AND
+                genre.$genre = 1";
+        } elseif ($text != "" && $genre == "") {
+            $filter = "story.Title LIKE '%$text%'";
+        } else {
+            $filter = "genre.$genre = 1";
+        }
+        
+		$results = mysqli_query($this->con, 
+            "SELECT 
+                story.Title, 
+                story.Identifier 
+            FROM 
+                story, 
+                genre 
+            WHERE 
+                $filter AND 
+                genre.Identifier = story.Identifier
+            GROUP BY Identifier
+            ORDER BY Views 
+            DESC LIMIT 10;");
+        if ($results=="") {
+            return null;
+        }
+        return $results;
+		//render_list($results);
+	}
+    
+    public function get_filtered_rated($text, $genre) {
+        $filter = "";
+        if ($text != "" && $genre != "") {
+            $filter = "story.Title LIKE '%$text%' AND
+                genre.$genre = 1";
+        } elseif ($text != "" && $genre == "") {
+            $filter = "story.Title LIKE '%$text%'";
+        } else {
+            $filter = "genre.$genre = 1";
+        }
+        
+		$results = mysqli_query($this->con, 
+            "SELECT 
+                story.Title, 
+                story.Identifier 
+            FROM 
+                story, 
+                genre 
+            WHERE 
+                $filter AND  
+                genre.Identifier = story.Identifier
+            GROUP BY Identifier
+            ORDER BY Sum_Of_Ratings / Num_Of_Ratings
+            DESC LIMIT 10;");
+        if ($results=="") {
+            return null;
+        }
+        return $results;
+		//render_list($results);
+	}
+    
+	public function get_filtered_newest($text, $genre) {
+        $filter = "";
+        if ($text != "" && $genre != "") {
+            $filter = "story.Title LIKE '%$text%' AND
+                genre.$genre = 1";
+        } elseif ($text != "" && $genre == "") {
+            $filter = "story.Title LIKE '%$text%'";
+        } else {
+            $filter = "genre.$genre = 1";
+        }
+        
+		$results = mysqli_query($this->con, 
+            "SELECT 
+                story.Title, 
+                story.Identifier 
+            FROM 
+                story, 
+                genre 
+            WHERE 
+                $filter AND 
+                genre.Identifier = story.Identifier
+            GROUP BY Identifier
+            ORDER BY Date
+            DESC LIMIT 10;");
+        if ($results=="") {
+            return null;
+        }
+        return $results;
+		//render_list($results);
+	}
+}
---
+++ cool_name/hw3/controllers/ReadController.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once("../configs/configs.php");
+require_once("../controllers/Controller.php");
+require_once("../models/Story.php");
+
+class ReadController extends Controller{
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function set_rating($id, $rate) {
+        mysqli_query($this->con, 
+             "UPDATE ratings 
+             SET Num_Of_Ratings = Num_Of_Ratings+1, Sum_Of_Ratings = Sum_Of_Ratings+'$rate'
+             WHERE Identifier='$id'"
+        );
+    }
+    
+    public function increment_view ($id) {
+        mysqli_query($this->con, "UPDATE story SET Views=Views+1 WHERE Identifier='$id'");
+        
+    }
+    
+    public function get_title() {
+        return $this->story->title;
+    }
+    
+    public function get_author() {
+        return $this->story->author;
+    }
+    
+    public function get_date() {
+        return $this->story->my_date;
+    }
+    
+    public function get_rating() {
+        if ($this->story->num_of_ratings == 0) {
+            return 0;
+        }
+        return $this->story->sum_of_ratings/$this->story->num_of_ratings;
+    }
+    
+    public function get_text() {
+        return $this->story->text;
+    }
+    
+}
